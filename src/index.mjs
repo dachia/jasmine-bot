812cd36ecaf0f3fc28d2f9edeccc61fbfc805e89
Artur Bartkevic <artur.bartkevic@gmail.com>
2024-03-03 17:05:29 +0000

    feat: listen to port

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,13 @@
 import 'dotenv/config'
 import express from 'express';
 import { startGrammy } from './startGrammy.mjs';
+import config from './config.mjs';
 
 startGrammy()
 
 const app = express()
 app.get('/', (req, res) => { res.send('Hello World!') })
-console.info("Express app started!")
+app.listen(config.PORT, () => {
+  console.log(`App listening at http://localhost:${config.PORT}`);
+});
+// console.info("Express app started!")

1029a6dac9df6bb7559f791b6f27a2cbe4e7eb80
Artur Bartkevic <artur.bartkevic@gmail.com>
2024-03-14 08:20:13 +0000

    feat: express serve frontend

--- packages/backend/src/express.mjs
+++ packages/backend/src/express.mjs
@@ -1,10 +1,21 @@
 import express from 'express';
-import config from './config.mjs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+// import config from './config.mjs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export function creteExpressApp() {
   return express()
 }
 
 export function registerExpressRoutes(app) {
-  app.get('/', (req, res) => { res.send('Hello World!') })
+  const staticPath = path.join(__dirname, '../../frontend/dist')
+  console.log(staticPath)
+  app.use(express.static(staticPath));
+  app.get('*', (req, res) => {
+    res.type('html')
+    res.sendFile(path.join(__dirname, '../../frontend/dist', 'index.html'));
+  });
 }
--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -7,6 +7,7 @@ import svgr from 'vite-plugin-svgr';
 // ----------------------------------------------------------------------
 
 export default defineConfig({
+  base: '/',
   plugins: [
     svgr({
       svgrOptions: {
